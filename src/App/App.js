8ece8df13f47f0528a82f496d4a810c9610ff4cb
timwatervoort <twwatervoort@gmail.com>
2021-10-16 14:54:27 -0600

    initial setup

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App game-text'>
+      <div className='container text-center pt-5'>
+        <div className='row'>
+          <div className='col'>
+            <h2 className='game-highlight'>Please enjoy</h2>
+            <h1 className='game-header'>A GAME.</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
